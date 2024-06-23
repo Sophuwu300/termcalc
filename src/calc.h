60d2e780edd34affdfcf4b553a5abd0473af0831
sophuwu <sophie@skisiel.com>
2024-06-23 14:19:22 +0200

    uwu

--- src/calc.h
+++ src/calc.h
@@ -1,6 +1,11 @@
 #ifndef TERMCALC_CALC_H
 #define TERMCALC_CALC_H
 
-
+#include <stdio.h>
+#include <float.h>
+#include <math.h>
+typedef long double flt_t;
+typedef long long int_t;
+#define print(s) printf("%s", s)
 
 #endif //TERMCALC_CALC_H
